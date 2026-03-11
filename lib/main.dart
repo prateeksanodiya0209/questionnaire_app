9412cdc1a17e3e5e613a1ab7c22f4b41df7e2fe9
Prateek Sanodiya <prateeksanodiya717@gmail.com>
2026-03-11 13:06:23 +0530

    Location Service enable and get lan & lon

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'app/routes/app_routes.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+
+  await Hive.openBox('submissions');
   await GetStorage.init();
 
   final box = GetStorage();
